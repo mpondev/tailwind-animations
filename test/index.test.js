3836aa6ae7e53182d86d3474bb4a2b0c2316a9eb
Mario Ponce <mario@mpondev.com>
2024-02-13 18:53:40 +0100

    Fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ describe('tailwindcss-animations plugins', () => {
     });
 
     expect(css).toMatch(
-      '@keyframes zoom-in{0%{opacity:0;transform:scale(.5)}100%{opacity:1;transform:scale(1)}}.animate-zoom-in{animation:zoom-in 0.6s ease-out}'
+      '@keyframes zoom-in{0%{opacity:0;transform:scale(.5)}100%{opacity:1;transform:scale(1)}}.animate-zoom-in{animation:zoom-in 0.6s ease-out both}'
     );
   });
 
