1dc48008f61e0c404ac43b99af832ae22433f131
Mario Ponce <mario@mpondev.com>
2024-02-14 18:48:04 +0100

    Add toggle button to animate all simultaneously

--- src/theme.js
+++ src/theme.js
@@ -17,7 +17,7 @@ export default {
     bouncing: 'bouncing 1s ease-in-out both',
     swing: 'swing 1s ease-in-out both',
     wobble: 'wobble 1s ease-in-out both',
-    pulsar: 'pulsar 1s ease-in-out both',
+    pulsing: 'pulsing 1s ease-in-out both',
     shake: 'shake 0.5s ease-in-out both',
     tada: 'tada 1s ease-in-out both',
     jump: 'jump 1s ease-in-out both',
@@ -73,6 +73,7 @@ export default {
     'bounce-fade-in': 'bounce-fade-in 0.6s ease-out both',
     'swing-drop-in': 'swing-drop-in 0.6s ease-out both',
     'pulse-fade-in': 'pulse-fade-in 0.6s ease-out both',
+    pulse: 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
   },
   keyframes: {
     'background-shine': {
@@ -154,7 +155,11 @@ export default {
       '75%': { transform: 'translateX(-5%)' },
       '100%': { transform: 'translateX(0)' },
     },
-    pulsar: {
+    pulse: {
+      '0%, 100%': { transform: 'opacity: 1' },
+      '50%': { transform: 'opacity: 0.5' },
+    },
+    pulsing: {
       '0%': { transform: 'scale(1)' },
       '50%': { transform: 'scale(1.1)' },
       '100%': { transform: 'scale(1)' },
