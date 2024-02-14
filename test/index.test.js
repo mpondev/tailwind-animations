48479a8d851d113c6e3c3077979a76e087affb6f
Mario Ponce <mario@mpondev.com>
2024-02-14 17:28:12 +0100

    Add animations play-state classes

--- test/index.test.js
+++ test/index.test.js
@@ -110,4 +110,12 @@ describe('tailwindcss-animations plugins', () => {
       '.animate-fill-mode-forwards{animation-fill-mode:forwards}'
     );
   });
+
+  it('use a play state animation play', async () => {
+    const css = await generatePluginCSS({
+      content: '<div class="animate-play-paused">Hello</div>',
+    });
+
+    expect(css).toMatch('.animate-play-paused{animation-play-state:paused}');
+  });
 });
